0f94cad7ef282f909c1ec29fa8b831bbf5cc047f
hanc3720 <colby.hanna@uleth.ca>
2025-11-02 13:53:27 -0700

    Created .h file with functions from UML

---
+++ include/IssueTrackerController.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "IssueRepository.h"
+
+class IssueTrackerController {
+ private:
+IssueRepository& repo;
+ public:
+    IssueTrackerController(IssueRepository& repository);
+
+    Issue createIssue(const std::string& title, const std::string& desc, const std::string& assignedTo);
+
+    bool updateIssueField(int id, const std::string& field, const std::string& value);
+
+    bool assignUserToIssue(int issueId, const std::string& userId);
+
+    bool unassignUserFromIssue(int issueId);
+
+    bool deleteIssue(int id);
+
+    Comment addCommentToIssue(int issueId, const std::string& text, const std::string& authorId);
+
+    bool updateComment(int commentId, const std::string& newText);
+
+    bool deleteComment(int commentId);
+
+    User createUser(const std::string& name);
+
+    bool removeUser(const std::string& id);
+
+    std::vector<Issue> listAllIssues();
+
+    std::vector<Issue> findIssuesByUserId(const std::string& userId);
+
+    std::vector<User> listAllUsers();
+};
+
+#endif
