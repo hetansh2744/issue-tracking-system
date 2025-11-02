8d7c58f8e6ddd196c73623ed14aa8bf96ba3a73f
hanc3720 <colby.hanna@uleth.ca>
2025-11-02 15:36:57 -0700

    updated Headers

--- src/IssueTrackerController.cpp
+++ src/IssueTrackerController.cpp
@@ -1,1 +1,13 @@
 #include "IssueTrackerController.h"
+
+IssueTrackerController::IssueTrackerController(IssueRepository& repository)
+    : repo(repository) {}
+
+Issue IssueTrackerController::createIssue(const string& title, const string& desc,
+const string& assignedTo){ 
+    if (title.empty() || desc.empty()) {
+        return Issue("", "", "");
+    }
+    Issue newIssue(title,desc,assignedTo);
+     return repo.saveIssue(newIssue);
+    }
