d9fcd06ed910e6d54c060809be4c508cc7596394
hanc3720 <colby.hanna@uleth.ca>
2025-11-03 16:32:33 -0700

    Redo mocks as they werent working, moved mock into test file as well to simplify

---
+++ test/IssueTrackerControllerTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "IssueTrackerController.h"
+
+class MockIssueRepository : public IssueRepository {
+public:
+    MOCK_METHOD(Issue, saveIssue, (const Issue& issue), (override));
+    MOCK_METHOD(Issue, getIssue, (int id), (override));
+    MOCK_METHOD(bool, deleteIssue, (int id), (override));
+    MOCK_METHOD(std::vector<Issue>, listIssues, (), (override));
+    MOCK_METHOD(std::vector<Issue>, findIssues, (const std::string& userId), (override));
+
+    MOCK_METHOD(Comment, saveComment, (const Comment& comment), (override));
+    MOCK_METHOD(Comment, getComment, (int id), (override));
+    MOCK_METHOD(bool, deleteComment, (int id), (override));
+
+    MOCK_METHOD(User, saveUser, (const User& user), (override));
+    MOCK_METHOD(User, getUser, (const std::string& id), (override));
+    MOCK_METHOD(bool, deleteUser, (const std::string& id), (override));
+    MOCK_METHOD(std::vector<User>, listAllUsers, (), (override));
+};
