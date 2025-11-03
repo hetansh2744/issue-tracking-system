0cc33d224ecf98f4579bef71758ef83403195071
hanc3720 <colby.hanna@uleth.ca>
2025-11-03 15:09:10 -0700

    Added methods to create, update and delete a comment

--- src/IssueTrackerController.cpp
+++ src/IssueTrackerController.cpp
@@ -83,3 +83,78 @@ std::vector<Issue> IssueTrackerController::listAllIssues() {
 std::vector<Issue> IssueTrackerController::findIssuesByUserId(const std::string& userId) {
     return repo->findIssues(userId);
 }
+Comment IssueTrackerController::addCommentToIssue(int issueId, const std::string& text, const std::string& authorId) {
+    try {
+        // 1. Validate
+        if (text.empty()) {
+            return Comment(0, "", "");
+        }
+        
+        // 2. Validate Issue and User existence
+        Issue issue = repo->getIssue(issueId);
+        repo->getUser(authorId); 
+
+        // 3. Create Comment
+        Comment newComment(issueId, authorId, text);
+        newComment.id = 0; // Signals to repository that its a new comment
+
+        // 4.Save Comment
+        Comment savedComment = repo->saveComment(newComment);
+
+        // 5. Update Issue
+        issue.addComment(savedComment.id);
+        repo->saveIssue(issue);
+        
+        return savedComment;
+
+    } catch (const out_of_range& e) {
+        // Issue or User not found
+        return Comment(0, "", "");
+    }
+}
+
+bool IssueTrackerController::updateComment(int commentId, const std::string& newText) {
+    try {
+        // 1. Validation
+        if (newText.empty()) {
+            return false;
+        }
+
+        // 2. Get Comment
+        Comment comment = repo->getComment(commentId);
+        
+        // 3. Update field
+        comment.text = newText;
+
+        // 4. Save Comment
+        repo->saveComment(comment);
+        return true;
+
+    } catch (const out_of_range& e) {
+        // Comment not found
+        return false;
+    }
+}
+
+bool IssueTrackerController::deleteComment(int commentId) {
+    try {
+        // 1. Get the comment to find its associated issueId
+        Comment comment = repo->getComment(commentId); 
+        int issueId = comment.issueId;
+
+        // 2. Delete the comment from the repository
+        bool deleted = repo->deleteComment(commentId);
+
+        if (deleted) {
+            // 3. Update the associated Issue to remove the comment ID
+            Issue issue = repo->getIssue(issueId); 
+            issue.removeComment(commentId); 
+            repo->saveIssue(issue);
+            return true;
+        }
+        return false;
+    } catch (const out_of_range& e) {
+        // Comment or Issue not found
+        return false;
+    }
+}
