5c3d6d891e0a3109cb727b6b9b8d5eb0e3928656
Fletcher (Fletcher) Coombs <coof3720@362mimas>
2025-11-04 11:29:25 -0700

    User class implementation

---
+++ src/User.cpp
@@ -0,0 +1,17 @@
+#include "User.h"
+
+std::string getName(){
+    return name;
+}
+
+std::string getRole(){
+    return role;
+}
+
+void setName(const std::string &newname){
+    name = newname;
+}
+
+void setRoll(const std::string &newroll){
+    roll = newroll;
+}
