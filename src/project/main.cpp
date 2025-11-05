56ea628c7ee2050514fc5319137597ab420cd65e
hanc3720 <colby.hanna@uleth.ca>
2025-11-05 10:47:45 -0700

    Added function in main so it'd pass static

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -4,5 +4,8 @@
 
 int main() {
     auto repo = createIssueRepository();
+    auto controller = std::make_unique<IssueTrackerController>(std::move(repo));
+    IssueTrackerView view(controller.get());
+    view.run();
     return 0;
 }
