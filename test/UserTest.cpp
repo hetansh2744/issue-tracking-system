ff865fb6c1e6e307568cb9789dfa49d02255e5f5
hanc3720 <colby.hanna@uleth.ca>
2025-11-06 02:04:29 -0700

    Fixed Style Issues

--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -1,12 +1,12 @@
-#include "User.hpp"
 #include <gtest/gtest.h>
+#include "User.hpp"
 
 TEST(UserTest, ConstructorAndGettersWork) {
     std::string expectedName = "Mario";
     std::string expectedRole = "Plumber/Hero";
-    
+
     User user(expectedName, expectedRole);
-    
+
     ASSERT_EQ(user.getName(), expectedName);
     ASSERT_EQ(user.getRole(), expectedRole);
 }
@@ -14,9 +14,9 @@ TEST(UserTest, ConstructorAndGettersWork) {
 TEST(UserTest, SetNameUpdatesName) {
     User user("Link", "Adventurer");
     std::string newName = "Zelda";
-    
+
     user.setName(newName);
-    
+
     ASSERT_EQ(user.getName(), newName);
     ASSERT_EQ(user.getRole(), "Adventurer");
 }
@@ -24,9 +24,9 @@ TEST(UserTest, SetNameUpdatesName) {
 TEST(UserTest, SetRoleUpdatesRole) {
     User user("Samus", "Hunter");
     std::string newRole = "Bounty Hunter";
-    
+
     user.setRole(newRole);
-    
+
     ASSERT_EQ(user.getRole(), newRole);
     ASSERT_EQ(user.getName(), "Samus");
 }
@@ -34,10 +34,10 @@ TEST(UserTest, SetRoleUpdatesRole) {
 TEST(UserTest, SetEmptyValues) {
     User user("Kirby", "Star Warrior");
     std::string emptyString = "";
-    
+
     user.setName(emptyString);
     user.setRole(emptyString);
-    
+
     ASSERT_EQ(user.getName(), emptyString);
     ASSERT_EQ(user.getRole(), emptyString);
 }
@@ -46,10 +46,10 @@ TEST(UserTest, VillainToHeroTransition) {
     User user("Bowser", "Villain");
     std::string newName = "Yoshi";
     std::string newRole = "Ally";
-    
+
     user.setName(newName);
     user.setRole(newRole);
-    
+
     ASSERT_EQ(user.getName(), newName);
     ASSERT_EQ(user.getRole(), newRole);
 }
