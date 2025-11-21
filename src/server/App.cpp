73ff9461a4c63c76c46b21639f0f1529844da933
foum3720 <miller.fourie@uleth.ca>
2025-11-21 16:13:26 -0700

    Add SwaggerComponent initialization and update connection provider comment

--- src/server/App.cpp
+++ src/server/App.cpp
@@ -1,17 +1,19 @@
 #include "AppComponent.hpp"
 #include "Runner.hpp"
+#include "SwaggerComponent.hpp"
 
+#include <oatpp/core/macro/component.hpp>
 #include <oatpp/network/tcp/server/ConnectionProvider.hpp>
 
 void run() {
 
   // Register Oat++ components
   AppComponent components;
+  SwaggerComponent swaggerComponents;
 
-  // Create server connection provider (localhost:8100)
-  auto connectionProvider =
-      oatpp::network::tcp::server::ConnectionProvider::createShared(
-          {"localhost", 8100, oatpp::network::Address::IP_4});
+  // Reuse the configured connection provider component (binds to 0.0.0.0:8100)
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::tcp::server::ConnectionProvider>,
+                  connectionProvider);
 
   // Create runner with provider
   Runner runner(connectionProvider);
