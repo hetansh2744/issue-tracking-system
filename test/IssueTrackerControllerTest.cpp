2575a7ba884a1a836f09f46d2ab37bd7f4fbeadc
foum3720 <miller.fourie@uleth.ca>
2025-11-22 21:19:41 -0700

    Style fixes

--- test/IssueTrackerControllerTest.cpp
+++ test/IssueTrackerControllerTest.cpp
@@ -35,7 +35,8 @@ class MockIssueRepository : public IssueRepository {
         (const, override));
     MOCK_METHOD(bool, deleteMilestone, (int milestoneId, bool cascade),
         (override));
-    MOCK_METHOD(std::vector<Milestone>, listAllMilestones, (), (const, override));
+    MOCK_METHOD(std::vector<Milestone>, listAllMilestones, (), 
+    (const, override));
     MOCK_METHOD(bool, addIssueToMilestone, (int milestoneId, int issueId),
         (override));
     MOCK_METHOD(bool, removeIssueFromMilestone, (int milestoneId, int issueId),
